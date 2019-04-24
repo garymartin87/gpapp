0f4fc26b76d9f3baefddaa1e98a81e6f2ec158e5
Gary Martin <alemmartin@gmail.com>
2019-04-24 13:41:30 -0300

    24-04-2019 - Create redux store

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reactThunk from 'redux-thunk';
 
+import reducers from './src/reducers';
 import Home from './src/components/Home';
 import BarcodeScanner from './src/components/BarcodeScanner';
 import AddProducts from './src/components/AddProducts';
-import { StyleSheet } from 'react-native';
 
 const AppNavigator = createStackNavigator(
     {
@@ -17,10 +20,21 @@ const AppNavigator = createStackNavigator(
     }
 );
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = [reactThunk];
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(...middlewares))
+);
+
 const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
     render() {
-        return <AppContainer />;
+        return (
+            <Provider store={store}>
+                <AppContainer />
+            </Provider>
+        );
     }
 }
