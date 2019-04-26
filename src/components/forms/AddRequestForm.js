6cff707e365e8fa89f18108a57e8171e68dc0472
Gary Martin <alemmartin@gmail.com>
2019-04-26 20:10:22 -0300

    26-04-2019 - WIP Add Products stable

--- src/components/forms/AddRequestForm.js
+++ src/components/forms/AddRequestForm.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { Button } from 'react-native-elements';
-import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { Field, FieldArray, reduxForm, arrayPush } from 'redux-form';
 
 import AddProduct from '../AddProduct';
 import RenderInput from '../RenderInput';
 
-const handleAddProduct = product => {
-    console.log('ADD PRODUCT code', product);
+const renderProduct = ({ item }) => {
+    return <Text>{item.code}</Text>;
+};
+
+const renderProducts = ({ fields, meta: { error, submitFailed } }) => {
+    return (
+        <FlatList
+            keyExtractor={item => item.code}
+            data={fields.getAll()}
+            renderItem={renderProduct}
+        />
+    );
 };
 
 let AddRequestForm = props => {
-    const { handleSubmit, onSubmit } = props;
+    const { handleSubmit, onSubmit, pushArray } = props;
+
+    const handleAddProduct = barcode => {
+        console.log('ADD PRODUCT code', barcode);
+        const product = { key: barcode, code: barcode };
+
+        pushArray('addProduct', 'products', product);
+    };
 
     return (
         <View style={styles.container}>
@@ -23,6 +41,7 @@ let AddRequestForm = props => {
             />
 
             <AddProduct onAddProduct={handleAddProduct} />
+            <FieldArray name="products" component={renderProducts} />
 
             <View style={styles.containerButton}>
                 <Button
@@ -56,9 +75,16 @@ const styles = StyleSheet.create({
     },
 });
 
+const mapDispatchToProps = {
+    pushArray: arrayPush,
+};
+
 AddRequestForm = reduxForm({
     // a unique name for the form
     form: 'addProduct',
 })(AddRequestForm);
 
-export default AddRequestForm;
+export default connect(
+    null,
+    mapDispatchToProps
+)(AddRequestForm);
